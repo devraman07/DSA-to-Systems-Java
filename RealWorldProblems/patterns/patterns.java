c704e817e44b696392bbf643677bb881ee374651
devraman07 <devraman07@gmail.com>
2026-04-23 16:43:44 +0530

    realworld problem done

--- RealWorldProblems/patterns/patterns.java
+++ RealWorldProblems/patterns/patterns.java
@@ -23,7 +23,21 @@ public class patterns {
         }
     }
 
+    public static void wareHouse() {
+        int n = 5;
+        int id = 1;
+        for(int i = 0; i<n; i++) {
+            for(int j = 0; j<=i; j++) {
+                System.out.print( "Pid: " + id + " " );
+                id++;
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
-        ServerLoad();
+        // ServerLoad();
+       
+        wareHouse();
     }
 }
